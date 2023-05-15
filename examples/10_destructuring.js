b8ec7eb6f269f38360761c55622ff272044277f4
Brandon Bruno <bmbruno@gmail.com>
2023-05-15 16:35:15 -0400

    Added Destructing example.

---
+++ examples/10_destructuring.js
@@ -0,0 +1,74 @@
+(function() {
+
+    /*
+    
+    Destructuring
+    
+    Unpacks values from an array or object into variables.
+    
+    */
+
+    window.Demo.Destructuring = function () {
+
+        console.clear();
+        console.log("DEMO: Destructuring");
+
+        //
+        // EXAMPLE 1: array destructuring
+        //
+
+        let numbers = [ 4, 7, 13, 22 ];
+
+        // Destructure the array into variables (variable names don't matter)
+        let [ a, b, c, d ] = numbers;
+
+        console.log(`a: ${a}, b: ${b}, c: ${c}, d: ${d}`);
+
+        // Destructures just the first two elements of the array
+        let [ e, f ] = numbers;
+        console.log(`e: ${e}, f: ${f}`);
+
+        //
+        // EXAMPLE 2: using rest operator to capture remaining values in array
+        //
+
+        let introduction = ["These", "are", "the", "voyages", "of", "the", "starship", "Enterprise" ];
+
+        // Capture first few elements, then gather the rest using the ... operator
+        let [ first, second, third, ...remainder ] = introduction;
+
+        console.log(`${first} ${second} ${third}`);
+        console.log(`${remainder}`);
+
+        //
+        // EXAMPLE 3: object destructuring
+        //
+
+        let sector = {
+            
+            number: "001",
+            name: "Solar System",
+            planets: [
+                {
+                    name: "Earth",
+                    size: 1.0
+                },
+                {
+                    name: "Mars",
+                    size: 0.53
+                }
+            ]
+        }
+
+        // Destructure properties from object (names do matter; order doesn't)
+        let { number, name, planets } = sector;
+
+        console.log(`number: ${number}`);
+        console.log(`name: ${name}`);
+        console.log(`First planet: ${planets[0].name}`);
+
+
+
+    }
+
+})();
