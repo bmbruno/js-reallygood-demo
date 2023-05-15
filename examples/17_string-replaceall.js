75b47a071192ad9206f7bee6c38caa60582f4987
Brandon Bruno <bmbruno@gmail.com>
2023-05-15 15:16:57 -0400

    Added stub code for remaining examples.

---
+++ examples/17_string-replaceall.js
@@ -0,0 +1,24 @@
+(function() {
+
+    /*
+    
+    XXXXXXXXXX
+    
+    Description
+    
+    */
+
+    window.Demo.XXXXXXXXXX = function () {
+
+        console.clear();
+        console.log("EXAMPLE: XXXXXXXXXX");
+
+        //
+        // EXAMPLE: Descriptions
+        //
+
+        
+
+    }
+
+})();
