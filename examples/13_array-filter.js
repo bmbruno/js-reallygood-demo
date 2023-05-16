2bb6515eb434f9595df55ac570f1be9bea9c834d
Brandon Bruno <bmbruno@gmail.com>
2023-05-16 10:37:29 -0400

    Added Array.filter example.

--- examples/13_array-filter.js
+++ examples/13_array-filter.js
@@ -4,20 +4,35 @@
     
     Array.filter
     
-    Description
+    filter() returns a new array containing elements that match the truthy condition of
+    the provided callback function.
     
     */
 
     window.Demo.ArrayFilter = function () {
 
         console.clear();
-        console.log("DEMO: Array.findIndex");
+        console.log("DEMO: Array.filter");
 
         //
-        // EXAMPLE: Descriptions
+        // EXAMPLE: filter only the bridge crew from this list
         //
 
-        
+        let crew = [
+            { name: "Jonathan", location: "Bridge" },
+            { name: "Hoshi", location: "Bridge" },
+            { name: "Trip", location: "Engineering" }
+        ];
+
+        console.log(crew);
+
+        let bridgeCrew = crew.filter( (element, index, array) => {
+
+            return element.location === "Bridge";
+
+        });
+
+        console.log(bridgeCrew);
 
     }
 
