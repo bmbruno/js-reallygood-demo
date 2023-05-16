5949b61fea1cfdb4709910e4c801691a6dbecf79
Brandon Bruno <bmbruno@gmail.com>
2023-05-16 11:19:28 -0400

    Added Classes example.

--- examples/14_classes.js
+++ examples/14_classes.js
@@ -4,7 +4,9 @@
     
     Classes
     
-    Description
+    JavaScript classes provide encapsulation for data and code. These are similar to classes 
+    in most modern object-oriented languages. Under the covers, classes use prototypes to
+    construct and manage their functionality.
     
     */
 
@@ -14,10 +16,41 @@
         console.log("DEMO: Classes");
 
         //
-        // EXAMPLE: Descriptions
+        // EXAMPLE: Build a "person" class with member and method
         //
 
+        class Person
+        {
+            location = "not set";
+
+            constructor( first, last ) {
+
+                this.firstName = first;
+                this.lastName = last;
+
+            }
+
+            getFullName() {
+                return `${this.firstName} ${this.lastName}`;
+            }
+
+            setLocation(location) {
+                this.location = location;
+            }
+        }
+
+        // Instantiate new instance of Person class
+        let captain = new Person("Christopher", "Pike");
         
+        // Access a member
+        console.log(captain.firstName);
+
+        // Call a method
+        console.log(captain.getFullName());
+
+        // Call another method and access related member
+        captain.setLocation("Bridge");
+        console.log(captain.location);
 
     }
 
