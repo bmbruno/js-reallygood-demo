428ddb861d44036cde95f4e31e7c0c4236516efb
Brandon Bruno <bmbruno@gmail.com>
2023-05-18 09:07:12 -0400

    Added generator example.

--- examples/15_generators.js
+++ examples/15_generators.js
@@ -4,7 +4,8 @@
     
     Generators
     
-    Description
+    A generator function is a type of iterator that 
+    is denoted with an asterisk after the function keyword (function*).
     
     */
 
@@ -14,10 +15,36 @@
         console.log("DEMO: Generators");
 
         //
-        // EXAMPLE: Descriptions
+        // EXAMPLE 1: This generator will produce sequential numbers infinity
         //
 
-        
+        function* IncrementInfinitely() {
+            
+            x = 0;
+            
+            while (true) {
+
+                x += 1;
+
+                // 'yield' will return the current value of x when .next() is called
+                // on this instance of the generator function
+                yield x;
+            }
+        }
+
+        // Instantiate function; this will create a generator that maintains state
+        let gen = IncrementInfinitely();
+
+        // .next() returns an object with two values:
+        //     'value' (whatever is being yielded)
+        //     'done' (true/false if last value has been yielded)
+        console.log(gen.next());
+
+        // Continue getting values from the generator function
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);
+        console.log(gen.next().value);    
 
     }
 
