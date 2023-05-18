2a593f246c9f8b71886902262ef7141735328698
Brandon Bruno <bmbruno@gmail.com>
2023-05-17 20:31:48 -0400

    Added String.ReplaceAll example.

--- examples/16_string-replaceall.js
+++ examples/16_string-replaceall.js
@@ -4,7 +4,8 @@
     
     String.replaceAll
     
-    Description
+    The String method replaceAll() replaces *all* instances of a string with another. This contrasts
+    to the older replace() method, which only replaced the first instance of a string.
     
     */
 
@@ -13,12 +14,19 @@
         console.clear();
         console.log("DEMO: String.replaceAll");
 
-        //
-        // EXAMPLE: Descriptions
-        //
+        let intro = "These are the voyages of the starship Enterprise.";
 
-        
+        // String.replace() only replaces first found instance
+        let newIntro1 = intro.replace("the", "THE");
+        console.log(newIntro1);
 
+        // Workaround: use a regular expression
+        let newIntro2 = intro.replace(new RegExp("the", "g"), "THE");
+        console.log(newIntro2);
+
+        // String.replaceAll() does exactly as you'd expect: replaces all instances of the string
+        let newIntro3 = intro.replaceAll("the", "THE");
+        console.log(newIntro3);
     }
 
 })();
